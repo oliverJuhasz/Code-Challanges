90396628f6bb7ed61c9842cfabf1404678a26454
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-10 18:11:26 +0200

    Make code faster by introducing a second hashtable

--- HackerRank/Java/src/main/java/FrequencyQueries/FrequencyQueries.java
+++ HackerRank/Java/src/main/java/FrequencyQueries/FrequencyQueries.java
@@ -1,9 +1,6 @@
 package FrequencyQueries;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class FrequencyQueries {
 
@@ -11,20 +8,32 @@ public class FrequencyQueries {
 
     public static List<Integer> freqQuery(int[][] queries) {
         Map<Integer, Integer> counter = new HashMap<>();
-        List<Integer> results = new ArrayList<>();
+        Map<Integer, Integer> occurances = new HashMap<>();
+        int xyz = 0;
+        List<Integer> results = new ArrayList<>(queries.length);
         for (int[] query : queries) {
+            xyz++;
             int command = query[0];
             int number = query[1];
             if (command == 1) {
-                counter.compute(number, (key, value) -> value == null ? 1 : value + 1);
+                if (counter.get(number) == null) {
+                    counter.put(number, 1);
+                    occurances.compute(1, (key, value) -> value != null ? value + 1 : 1);
+                } else {
+                    int frequency = counter.get(number);
+                    counter.put(number, frequency + 1);
+                    occurances.compute(frequency, (key, value) -> value - 1);
+                    occurances.compute(frequency + 1, (key, value) -> value != null ? value + 1 : 1);
+                }
             } else if (command == 2) {
                 Integer occurrenceToRemove = counter.get(number);
                 if (occurrenceToRemove != null && occurrenceToRemove != 0) {
                     counter.compute(number, (key, value) -> value - 1);
+                    occurances.compute(occurrenceToRemove, (key, value) -> value > 0 ? value - 1 : 0);
+                    occurances.compute(occurrenceToRemove - 1, (key, value) -> value != null ? value + 1 : 1);
                 }
             } else {
-                boolean validMatch = counter.values().stream().anyMatch(value -> value == number);
-                if (validMatch) {
+                if (occurances.get(number) != null && occurances.get(number) > 0) {
                     results.add(1);
                 } else {
                     results.add(0);

56c102b67a63dcf77dd241e6c5657f9ce52f4664
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-10 11:17:11 +0200

    Add new exercise skeleton

---
+++ HackerRank/Java/src/main/java/FrequencyQueries/FrequencyQueries.java
@@ -0,0 +1,15 @@
+package FrequencyQueries;
+
+import java.util.List;
+
+public class FrequencyQueries {
+
+    //https://www.hackerrank.com/challenges/frequency-queries/
+
+    // Complete the freqQuery function below.
+    static List<Integer> freqQuery(List<List<Integer>> queries) {
+        return null;
+
+    }
+
+}

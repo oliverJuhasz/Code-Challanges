71445cb578a22bb1ee331072c650ff12359dd40b
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-10 18:47:38 +0200

    Upload exercise skeleton

---
+++ HackerRank/Java/src/main/java/SherlockAndAnagrams/SherlockAndAnagrams.java
@@ -0,0 +1,14 @@
+package SherlockAndAnagrams;
+
+public class SherlockAndAnagrams {
+
+    //https://www.hackerrank.com/challenges/sherlock-and-anagrams/
+
+    // Complete the sherlockAndAnagrams function below.
+    static int sherlockAndAnagrams(String s) {
+        return 1;
+
+    }
+
+
+}

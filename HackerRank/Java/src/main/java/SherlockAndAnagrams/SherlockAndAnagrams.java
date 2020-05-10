bb631edf02e1212aeeda1079f6db2a2999d060dc
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-10 19:18:27 +0200

    Split parts of word to substrings

--- HackerRank/Java/src/main/java/SherlockAndAnagrams/SherlockAndAnagrams.java
+++ HackerRank/Java/src/main/java/SherlockAndAnagrams/SherlockAndAnagrams.java
@@ -1,13 +1,30 @@
 package SherlockAndAnagrams;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class SherlockAndAnagrams {
 
     //https://www.hackerrank.com/challenges/sherlock-and-anagrams/
 
     // Complete the sherlockAndAnagrams function below.
-    public static int sherlockAndAnagrams(String s) {
+    public static int sherlockAndAnagrams(String word) {
+        Map<String, Integer> substrings = splitToSubstrings(word);
         return 1;
 
+    }
+
+    private static Map<String, Integer> splitToSubstrings(String word) {
+        Map<String, Integer> substrings = new HashMap<>();
+        for (int i = 1; i < word.length(); i++) {
+            for (int j = 0; j <= word.length() - i; j++) {
+                String subString = word.substring(j, j + i);
+                substrings.compute(subString, (key, value) -> value != null ? value + 1 : 1);
+            }
+        }
+        return substrings;
     }
 
 

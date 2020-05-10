f7c03a775ae6403ef1450f3b9f1950711fa95113
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-10 12:26:34 +0200

    Add basic solution

--- HackerRank/Java/src/main/java/FrequencyQueries/FrequencyQueries.java
+++ HackerRank/Java/src/main/java/FrequencyQueries/FrequencyQueries.java
@@ -1,6 +1,9 @@
 package FrequencyQueries;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FrequencyQueries {
 
@@ -8,7 +11,28 @@ public class FrequencyQueries {
 
     // Complete the freqQuery function below.
     public static List<Integer> freqQuery(List<List<Integer>> queries) {
-        return null;
+        Map<Integer, Integer> counter = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        for (List<Integer> query : queries) {
+            int command = query.get(0);
+            int number = query.get(1);
+            if (command == 1) {
+                counter.compute(number, (key, value) -> value == null ? 1 : value + 1);
+            } else if (command == 2) {
+                Integer occuranceToRemove = counter.get(number);
+                if (occuranceToRemove != null) {
+                    counter.compute(number, (key, value) -> value - 1);
+                }
+            } else {
+                boolean validMatch = counter.values().stream().anyMatch(value -> value == number);
+                if (validMatch) {
+                    result.add(1);
+                } else {
+                    result.add(0);
+                }
+            }
+        }
+        return result;
 
     }
 

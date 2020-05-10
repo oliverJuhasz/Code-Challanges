77d5dbf1c057e505dbc0d60064c16787cb08abb4
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-10 10:42:43 +0200

    Apply different logic

--- HackerRank/Java/src/main/java/CountTriplets.java
+++ HackerRank/Java/src/main/java/CountTriplets.java
@@ -4,30 +4,25 @@ import java.util.Map;
 
 public class CountTriplets {
 
-    static long countTriplets(List<Long> numbers, long ratio) {
-        long numberOfTriplets = 0;
-        Map<Long, Long> countPerNumber = countNumbersInList(numbers);
-        for (Long number : countPerNumber.keySet()) {
-            long multipliedOnce = number * ratio;
-            long multipliedTwice = number * ratio * ratio;
-
-            long firstMemberOccurance = countPerNumber.get(number);
-            long secondMemberOccurance = countPerNumber.get(multipliedOnce) == null ? 0 : countPerNumber.get(multipliedOnce);
-            long thirdMemberOccurance = countPerNumber.get(multipliedTwice) == null ? 0 : countPerNumber.get(multipliedTwice);
-            numberOfTriplets += firstMemberOccurance * secondMemberOccurance * thirdMemberOccurance;
+    static long countTriplets(List<Long> numbers, long ratio){
+        Map<Long,Long> leftPart = new HashMap<>();
+        Map<Long,Long> midPart = new HashMap<>();
+        long numberOfTriplets = 0L;
+        for (Long number : numbers) {
+            numberOfTriplets += midPart.getOrDefault(number, 0L);
+            addMidPartIfLeftPartExists(ratio, leftPart, midPart, number);
+            addOrIncrementLeftPart(ratio, leftPart, number);
         }
         return numberOfTriplets;
     }
 
-    private static Map<Long, Long> countNumbersInList(List<Long> numbers) {
-        Map<Long, Long> countPerNumber = new HashMap<>();
-        for (Long number : numbers) {
-            if (countPerNumber.containsKey(number)) {
-                countPerNumber.put(number, countPerNumber.get(number) + 1);
-            } else {
-                countPerNumber.put(number, 1L);
-            }
+    private static void addMidPartIfLeftPartExists(long ratio, Map<Long, Long> leftPart, Map<Long, Long> midPart, Long number) {
+        if (leftPart.get(number)!=null) {
+            midPart.compute(number * ratio, (key,value) -> value != null ? value + leftPart.get(number) : leftPart.get(number));
         }
-        return countPerNumber;
+    }
+
+    private static void addOrIncrementLeftPart(long ratio, Map<Long, Long> leftPart, Long number) {
+        leftPart.compute(number * ratio, (key,value) -> value == null ? 1 : value + 1);
     }
 }

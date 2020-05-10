75a7bc4df3a19d8024c623c8b461eff6e9bffcb5
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-10 18:22:51 +0200

    Fix typo

--- HackerRank/Java/src/main/java/FrequencyQueries/FrequencyQueries.java
+++ HackerRank/Java/src/main/java/FrequencyQueries/FrequencyQueries.java
@@ -8,7 +8,7 @@ public class FrequencyQueries {
 
     public static List<Integer> freqQuery(int[][] queries) {
         Map<Integer, Integer> counter = new HashMap<>();
-        Map<Integer, Integer> occurances = new HashMap<>();
+        Map<Integer, Integer> occurrences = new HashMap<>();
         List<Integer> results = new ArrayList<>(queries.length);
         for (int[] query : queries) {
             int command = query[0];
@@ -16,22 +16,22 @@ public class FrequencyQueries {
             if (command == 1) {
                 if (counter.get(number) == null) {
                     counter.put(number, 1);
-                    occurances.compute(1, (key, value) -> value != null ? value + 1 : 1);
+                    occurrences.compute(1, (key, value) -> value != null ? value + 1 : 1);
                 } else {
                     int frequency = counter.get(number);
                     counter.put(number, frequency + 1);
-                    occurances.compute(frequency, (key, value) -> value - 1);
-                    occurances.compute(frequency + 1, (key, value) -> value != null ? value + 1 : 1);
+                    occurrences.compute(frequency, (key, value) -> value - 1);
+                    occurrences.compute(frequency + 1, (key, value) -> value != null ? value + 1 : 1);
                 }
             } else if (command == 2) {
                 Integer occurrenceToRemove = counter.get(number);
                 if (occurrenceToRemove != null && occurrenceToRemove != 0) {
                     counter.compute(number, (key, value) -> value - 1);
-                    occurances.compute(occurrenceToRemove, (key, value) -> value > 0 ? value - 1 : 0);
-                    occurances.compute(occurrenceToRemove - 1, (key, value) -> value != null ? value + 1 : 1);
+                    occurrences.compute(occurrenceToRemove, (key, value) -> value > 0 ? value - 1 : 0);
+                    occurrences.compute(occurrenceToRemove - 1, (key, value) -> value != null ? value + 1 : 1);
                 }
             } else {
-                if (occurances.get(number) != null && occurances.get(number) > 0) {
+                if (occurrences.get(number) != null && occurrences.get(number) > 0) {
                     results.add(1);
                 } else {
                     results.add(0);

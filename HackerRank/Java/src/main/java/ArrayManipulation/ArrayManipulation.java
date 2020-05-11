5e984399e1f4a8e88fbe991f1433689046d11e01
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-11 16:03:31 +0200

    Add skeleton code

---
+++ HackerRank/Java/src/main/java/ArrayManipulation/ArrayManipulation.java
@@ -0,0 +1,11 @@
+package ArrayManipulation;
+
+public class ArrayManipulation {
+
+    //https://www.hackerrank.com/challenges/crush/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+    static long arrayManipulation(int n, int[][] queries) {
+        return 1L;
+    }
+
+}

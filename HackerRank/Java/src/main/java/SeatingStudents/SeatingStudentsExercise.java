9175fa5f18655374ee92283156a9ed6194a09836
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-12 12:57:01 +0200

    Do renaming

--- HackerRank/Java/src/main/java/SeatingStudents/SeatingStudentsExercise.java
+++ HackerRank/Java/src/main/java/SeatingStudents/SeatingStudentsExercise.java
@@ -10,40 +10,33 @@ public class SeatingStudentsExercise {
     public static int SeatingStudents(int[] input) {
         int possibleCombinations = 0;
         int numberOfSeating = input[0];
-        Set<Integer> occupiedDeskIndexes = new HashSet<>();
+        Set<Integer> invalidDeskIndexes = new HashSet<>();
         Arrays.stream(input)
                 .skip(1L)
                 .map(value -> value - 1)
-                .forEach(occupiedDeskIndexes::add);
+                .forEach(invalidDeskIndexes::add);
+        invalidDeskIndexes.addAll(List.of(-1, numberOfSeating, numberOfSeating + 1));
         boolean isDeskIndexOdd = true;
         for (int i = 1; i < numberOfSeating; i += isDeskIndexOdd ? 3 : 1) {
-            boolean isExaminedDeskOccupied = occupiedDeskIndexes.contains(i);
+            boolean isExaminedDeskOccupied = invalidDeskIndexes.contains(i);
             if (!isExaminedDeskOccupied) {
-                possibleCombinations += getNumberOfSeatingCombinationForSeat(isDeskIndexOdd, i, occupiedDeskIndexes, numberOfSeating);
+                possibleCombinations += getNumberOfSeatingCombinationForSeat(isDeskIndexOdd, i, invalidDeskIndexes);
             }
             isDeskIndexOdd = !isDeskIndexOdd;
         }
         return possibleCombinations;
     }
 
-    private static int getNumberOfSeatingCombinationForSeat(boolean evenDeskNumber, int i, Set<Integer> occupiedDesks, int numberOfSeating) {
+    private static int getNumberOfSeatingCombinationForSeat(boolean isDeskIndexOdd, int i, Set<Integer> invalidDeskIndexes) {
         int possibleCombinations = 0;
-        if (evenDeskNumber) {
-            if (i - 2 >= 0) {
-                possibleCombinations += occupiedDesks.contains(i - 2) ? 0 : 1;
-            }
-            if (i + 1 <= numberOfSeating) {
-                possibleCombinations += occupiedDesks.contains(i - 1) ? 0 : 1;
-            }
-            if (i + 2 <= numberOfSeating) {
-                possibleCombinations += occupiedDesks.contains(i + 2) ? 0 : 1;
-            }
+        if (isDeskIndexOdd) {
+            possibleCombinations += invalidDeskIndexes.contains(i - 2) ? 0 : 1;
+            possibleCombinations += invalidDeskIndexes.contains(i - 1) ? 0 : 1;
+            possibleCombinations += invalidDeskIndexes.contains(i + 2) ? 0 : 1;
         } else {
-            possibleCombinations += occupiedDesks.contains(i - 2) ? 0 : 1;
-            possibleCombinations += occupiedDesks.contains(i + 1) ? 0 : 1;
-            if (i + 2 < numberOfSeating) {
-                possibleCombinations += occupiedDesks.contains(i + 2) ? 0 : 1;
-            }
+            possibleCombinations += invalidDeskIndexes.contains(i - 2) ? 0 : 1;
+            possibleCombinations += invalidDeskIndexes.contains(i + 1) ? 0 : 1;
+            possibleCombinations += invalidDeskIndexes.contains(i + 2) ? 0 : 1;
         }
         return possibleCombinations;
     }

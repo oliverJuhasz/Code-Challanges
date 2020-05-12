dacf8d7f0c48c18e660e395a753eb777568b0ed3
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-12 12:44:42 +0200

    Refactor exercise

--- HackerRank/Java/src/main/java/SeatingStudents/SeatingStudentsExercise.java
+++ HackerRank/Java/src/main/java/SeatingStudents/SeatingStudentsExercise.java
@@ -16,28 +16,32 @@ public class SeatingStudentsExercise {
                 .forEach(occupiedDesks::add);
         boolean evenDeskNumber = true;
         for (int i = 1; i < numberOfSeating; i += evenDeskNumber ? 3 : 1) {
-            if (occupiedDesks.contains(i + 1)) {
-                evenDeskNumber = !evenDeskNumber;
-                continue;
+            boolean isExaminedDeskOccupied = occupiedDesks.contains(i + 1);
+            if (!isExaminedDeskOccupied) {
+                possibleCombinations += getNumberOfSeatingCombinationForSeat(evenDeskNumber, i, occupiedDesks, numberOfSeating);
             }
-            if (evenDeskNumber) {
-                if (i - 2 >= 0) {
-                    possibleCombinations += occupiedDesks.contains(i - 1) ? 0 : 1;
-                }
-                if (i + 1 <= numberOfSeating) {
-                    possibleCombinations += occupiedDesks.contains(i) ? 0 : 1;
-                }
-                if (i + 2 <= numberOfSeating) {
-                    possibleCombinations += occupiedDesks.contains(i + 3) ? 0 : 1;
-                }
-                evenDeskNumber = false;
-            } else {
+            evenDeskNumber = !evenDeskNumber;
+        }
+        return possibleCombinations;
+    }
+
+    private static int getNumberOfSeatingCombinationForSeat(boolean evenDeskNumber, int i, Set<Integer> occupiedDesks, int numberOfSeating) {
+        int possibleCombinations = 0;
+        if (evenDeskNumber) {
+            if (i - 2 >= 0) {
                 possibleCombinations += occupiedDesks.contains(i - 1) ? 0 : 1;
-                possibleCombinations += occupiedDesks.contains(i + 2) ? 0 : 1;
-                if (i + 2 < numberOfSeating) {
-                    possibleCombinations += occupiedDesks.contains(i + 3) ? 0 : 1;
-                }
-                evenDeskNumber = true;
+            }
+            if (i + 1 <= numberOfSeating) {
+                possibleCombinations += occupiedDesks.contains(i) ? 0 : 1;
+            }
+            if (i + 2 <= numberOfSeating) {
+                possibleCombinations += occupiedDesks.contains(i + 3) ? 0 : 1;
+            }
+        } else {
+            possibleCombinations += occupiedDesks.contains(i - 1) ? 0 : 1;
+            possibleCombinations += occupiedDesks.contains(i + 2) ? 0 : 1;
+            if (i + 2 < numberOfSeating) {
+                possibleCombinations += occupiedDesks.contains(i + 3) ? 0 : 1;
             }
         }
         return possibleCombinations;

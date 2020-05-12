a1ea51f44b9a57f8aa47f7e08482ae707e42635f
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-12 12:47:54 +0200

    Map seat numbers to index

--- HackerRank/Java/src/main/java/SeatingStudents/SeatingStudentsExercise.java
+++ HackerRank/Java/src/main/java/SeatingStudents/SeatingStudentsExercise.java
@@ -13,10 +13,11 @@ public class SeatingStudentsExercise {
         Set<Integer> occupiedDesks = new HashSet<>();
         Arrays.stream(input)
                 .skip(1L)
+                .map(value -> value - 1)
                 .forEach(occupiedDesks::add);
         boolean evenDeskNumber = true;
         for (int i = 1; i < numberOfSeating; i += evenDeskNumber ? 3 : 1) {
-            boolean isExaminedDeskOccupied = occupiedDesks.contains(i + 1);
+            boolean isExaminedDeskOccupied = occupiedDesks.contains(i);
             if (!isExaminedDeskOccupied) {
                 possibleCombinations += getNumberOfSeatingCombinationForSeat(evenDeskNumber, i, occupiedDesks, numberOfSeating);
             }
@@ -29,19 +30,19 @@ public class SeatingStudentsExercise {
         int possibleCombinations = 0;
         if (evenDeskNumber) {
             if (i - 2 >= 0) {
-                possibleCombinations += occupiedDesks.contains(i - 1) ? 0 : 1;
+                possibleCombinations += occupiedDesks.contains(i - 2) ? 0 : 1;
             }
             if (i + 1 <= numberOfSeating) {
-                possibleCombinations += occupiedDesks.contains(i) ? 0 : 1;
+                possibleCombinations += occupiedDesks.contains(i - 1) ? 0 : 1;
             }
             if (i + 2 <= numberOfSeating) {
-                possibleCombinations += occupiedDesks.contains(i + 3) ? 0 : 1;
+                possibleCombinations += occupiedDesks.contains(i + 2) ? 0 : 1;
             }
         } else {
-            possibleCombinations += occupiedDesks.contains(i - 1) ? 0 : 1;
-            possibleCombinations += occupiedDesks.contains(i + 2) ? 0 : 1;
+            possibleCombinations += occupiedDesks.contains(i - 2) ? 0 : 1;
+            possibleCombinations += occupiedDesks.contains(i + 1) ? 0 : 1;
             if (i + 2 < numberOfSeating) {
-                possibleCombinations += occupiedDesks.contains(i + 3) ? 0 : 1;
+                possibleCombinations += occupiedDesks.contains(i + 2) ? 0 : 1;
             }
         }
         return possibleCombinations;

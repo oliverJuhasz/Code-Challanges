9dc23ec30ad5ea1b24ab4c7620d792b25e0d5c56
Oliver Juhasz <oliver.juhasz@outlook.com>
2020-05-16 12:28:10 +0200

    Add solution based on hashtable

---
+++ HackerRank/Java/src/main/java/SparseArrays/SparseArrays.java
@@ -0,0 +1,20 @@
+package SparseArrays;
+
+import java.util.HashMap;
+
+public class SparseArrays {
+
+    //https://www.hackerrank.com/challenges/sparse-arrays/problem
+
+    public static int[] matchingStrings(String[] strings, String[] queries) {
+        HashMap<String, Integer> counter = new HashMap<>();
+        int[] results = new int[queries.length];
+        for (String word : strings) {
+            counter.compute(word, (key, value) -> counter.containsKey(word) ? value + 1 : 1);
+        }
+        for (int i = 0; i < queries.length; i++) {
+            results[i] = counter.getOrDefault(queries[i], 0);
+        }
+        return results;
+    }
+}
